46a034d8c5bbfdf9223fd6160a3573ba7c496961
robertjosephderouin <robertjosephderouin@gmail.com>
2021-05-26 08:25:12 -0700

    Add api call on mount

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getReservations } from './apiCalls';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: [],
+    }
+  }
+
+  componentDidMount = () => {
+  getReservations()
+    .then(data => {
+      this.setState({ reservations: data })
+    })
+    .catch(() => this.setState({ error: 'Something went wrong'}))
+}
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
         </div>
         <div className='resy-container'>
-          
         </div>
       </div>
     )
